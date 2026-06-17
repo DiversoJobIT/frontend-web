25b28d6822b962a90ee74e939b8b8603e35926b0
Alejo <alejodev2022@gmail.com>
2026-06-17 17:59:02 -0500

    feat: agregar componente Blog a la página de destino

---
+++ app/landing/components/Blog.tsx
@@ -0,0 +1,34 @@
+
+export default function Blog() {
+  return (
+    <div>
+      {/* Section Title */}
+      <div>
+        <h3>
+          Nuestro Blog
+        </h3>
+        <span>Aprende sobre inclucion laboral y crecimiento profecional</span>
+
+
+      </div>
+
+        {/* Blog Posts Grid */}
+      <div>
+        <article>
+          <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&q=80&w=1200" alt="Blog Post 1" />
+          <h4>Bienvenido a diversoJob : Donde el talento no tiene barreras</h4>
+          <p>por: Angie</p>
+          <p>Marzo 19, 2026</p>
+          <p>En DiversoJob tenemos una convicción clara: el talento no entiende de límites, solo de capacidades. No somos</p>
+          <a href="#">Leer más</a>
+        </article>
+
+
+
+
+
+      </div>
+    </div>
+
+      )
+}
